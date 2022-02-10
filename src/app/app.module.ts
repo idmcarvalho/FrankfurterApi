24d087609f064218a439c304dea043b98f51ab89
Mateus Marques <m.marques@intcom.com.br>
2022-02-09 21:57:18 -0300

    Configuração inicial appModule.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { NavMenuComponent } from './nav-menu/nav-menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavMenuComponent,
+    MenuComponent
   ],
   imports: [
-    BrowserModule
+    FormsModule,
+    BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
+    RouterModule.forRoot([
+      { path: '', component: MenuComponent, pathMatch: 'full' },
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
