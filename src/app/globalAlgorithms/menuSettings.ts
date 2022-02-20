4323f812fe7f710b9f21f6faab5a0d6ff29ff30e
Mateus Marques <mat.marq.oliveira@gmail.com>
2022-02-20 11:36:19 -0300

    Adição descrição cards

--- src/app/globalAlgorithms/menuSettings.ts
+++ src/app/globalAlgorithms/menuSettings.ts
@@ -9,8 +9,8 @@ export class menuSettings {
                 // statement 1
                 this.cards = {
                     title: 'Frankfurter Api',
-                    description: 'Use the buttons beside to search about currencies',
-                    footer: 'ToDo',
+                    description: 'The Frankfurter API tracks foreign exchange references rates published by the European Central Bank. The data refreshes around 16:00 CET every working day. Frankfurter integrates seamlessly with libraries like Money.js and Dinero.js.',
+                    footer: 'https://api.frankfurter.app/${parameters}',
                     photo: '../../assets/cards/menu.png'
                 }
                 break;
@@ -18,8 +18,8 @@ export class menuSettings {
                 // statement 2
                 this.cards = {
                     title: 'Latest',
-                    description: 'Get the latest rates from the main coins around the world',
-                    footer: 'ToDo',
+                    description: 'This endpoint returns the latest rates. We added a feature to export these rates to excel, so it is possible to get these dates and create an analysis',
+                    footer: 'https://api.frankfurter.app/latest',
                     photo: '../../assets/cards/latest.png'
                 }
                 break;
@@ -27,8 +27,8 @@ export class menuSettings {
                 // statement 3
                 this.cards = {
                     title: 'Historical',
-                    description: 'Get the latest rates from the main coins around the world',
-                    footer: 'ToDo',
+                    description: 'This endpoint returns historical rates for any working day since 4 January 1999.',
+                    footer: 'https://api.frankfurter.app/1999-01-04',
                     photo: '../../assets/cards/historical.png'
                 }
                 break;
@@ -36,20 +36,20 @@ export class menuSettings {
                 // statement 4
                 this.cards = {
                     title: 'Time Series',
-                    description: 'Get the latest rates from the main coins around the world',
-                    footer: 'ToDo',
+                    description: 'Be careful when choosing very long periods, in some tests the browser crashed and had to be restarted. The longest period from 1999 to today returns around 36,000 rows.',
+                    footer: 'https://api.frankfurter.app/2020-01-01..2020-01-31',
                     photo: '../../assets/cards/timeSeries.png'
                 }
                 break;
-                case 'conversion':
-                    // statement 5
-                    this.cards = {
-                        title: 'Conversion',
-                        description: 'Get the latest rates from the main coins around the world',
-                        footer: 'ToDo',
-                        photo: '../../assets/cards/conversion.png'
-                    }
-                    break;
+            case 'conversion':
+                // statement 5
+                this.cards = {
+                    title: 'Conversion',
+                    description: 'You can convert any value between currencies using the above endpoints in combination with the amount parameter.',
+                    footer: 'https://api.frankfurter.app/latest/?amount=10&from=GBP&to=USD',
+                    photo: '../../assets/cards/conversion.png'
+                }
+                break;
             default:
                 // 
                 break;
